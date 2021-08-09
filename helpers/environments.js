3863ca33836f109edf3d704c3296c3bdffc88149
aboutsabbir <sabbir.ruet15@gmail.com>
2021-08-09 18:23:25 +0600

    environments variables are completed

---
+++ helpers/environments.js
@@ -0,0 +1,31 @@
+/*
+* Title: Environments
+* Description: Handle all environment related things
+* Author: Md. Sabbir Ahmed
+* Date: 09/08/2021
+*/
+
+//dependencies
+
+
+// module scaffolding
+const environments = {};
+
+environments.staging = {
+    port: 3000,
+    envName: 'staging'
+};
+
+environments.production = {
+    port: 5000,
+    envName: 'production'
+}
+
+// determine which environement was passed
+const currentEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : 'staging';
+
+// export correspondingg environment object
+const environmentToExport = typeof(environments[currentEnvironment]) === 'object' ? environments[currentEnvironment] : environments.staging;
+
+// export module
+module.exports = environmentToExport;
